a17a5d91b180f0234178f6f3eb0bd8f511e586b1
nicaho <rungege520@163.com>
2024-05-02 15:48:24 +0800

    Initial Commit

---
+++ LoadDataDemo/LoadDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadDataDemoApp.swift
+//  LoadDataDemo
+//
+//  Created by nicaho on 2024/5/2.
+//
+
+import SwiftUI
+
+@main
+struct LoadDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
